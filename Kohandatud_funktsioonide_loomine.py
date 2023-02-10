0df3735d5d1cb6bb7bc3e2628ef8780089dc2b7b
opilane <opilane@HP-CZC2349HTZ>
2023-02-10 13:59:29 +0200

    Add project files.

---
+++ Kohandatud_funktsioonide_loomine.py
@@ -0,0 +1,22 @@
+#10/02/23
+#1
+from math import *
+from random import *
+from OmaModul import *
+#1
+while True:
+    vastus=arithmetic(float(input("arv1:")),input("tehe:"),float(input("arv2:")))
+    print(vastus)
+
+#2
+vastus=is_year_leap(input("Aasta:"))
+print(vastus)
+#või
+print(is_year_leap(input("Aasta:")))
+
+   
+
+
+
+
+
---
+++ OmaModul.py
@@ -0,0 +1,30 @@
+﻿from math import *
+from pickle import FALSE, TRUE
+from random import *
+from OmaModul import *
+def arithmetic(arv1:float,tehe:str,arv2:float)->any:
+    """
+    """
+    if tehe=="+":
+        vastus=arv1+arv2
+    elif tehe=="-":
+        vastus=arv1-arv2
+    elif tehe=="*":
+        vastus=arv1*arv2
+    elif tehe=="/":
+        if arv2==0:
+            vastus="DIVO"
+        else:
+            vasts=arv1/arv2
+    else:
+        vastus="Tundmatu tehe"
+    return vastus
+#2
+def is_year_leap(aasta: int)->bool:
+    """
+    """
+    if aasta%4==0:
+        t=True 
+    else:
+        t=False 
+    return t
